1a4002f80e895c5a0beaeff3731a5e708e22b856
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-01-20 21:56:22 +0100

    implemet user authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "search", to: "search#index"
+  # get "show", to: "search#show"
+ 
   root "articles#index"
 
   resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :search
+  
 end
