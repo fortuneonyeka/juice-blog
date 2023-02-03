833c457d8989dd41570dc0ba0b86b67e3e12ee72
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-02-03 11:58:00 +0100

    update search before_action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
     @query = Article.ransack(params[:q])
     @articles = @query.result(distinct: true)
